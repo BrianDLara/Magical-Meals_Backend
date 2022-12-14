2a1083c1309ee6bf0c986c59d64e4be2c481204f
Brian Lara <Briandlara1@gmail.com>
2022-12-14 09:44:37 -0500

    updated Updatepassword route

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -4,8 +4,9 @@ const middleware = require('../middleware')
 
 Router.post('/register', controller.Register)
 Router.post('/login', controller.Login)
+
 Router.put(
-  '/update',
+  '/update_password/user_id/:user_id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.UpdatePassword
